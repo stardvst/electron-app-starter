f5a70017569dceb039223e075b4be33ef4728c2e
stardvst <lilia.manukyan@hotmail.com>
2023-09-22 11:10:33 +0400

    added event listener to close the app or create window if none exist

--- main.js
+++ main.js
@@ -11,4 +11,22 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  // windows cannot be created before the ready event
+  // listen for activate events after your app is initialized
+  app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0) {
+      // macOS apps generally continue running even without any windows open
+      // Activating the app when no windows are available should open a new one
+      createWindow()
+    }
+  })
+})
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on('window-all-closed', () => {
+  // exit app if the user is not on macOS
+  if(process.platform !== 'darwin') {
+    app.quit()
+  }
 })
